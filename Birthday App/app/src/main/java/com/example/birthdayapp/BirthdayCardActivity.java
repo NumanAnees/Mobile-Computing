e8a8fb3452d0011c67de34ea4b47840111a5682e
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-04-23 14:41:40 +0500

    final

--- Birthday App/app/src/main/java/com/example/birthdayapp/BirthdayCardActivity.java
+++ Birthday App/app/src/main/java/com/example/birthdayapp/BirthdayCardActivity.java
@@ -3,12 +3,17 @@ package com.example.birthdayapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BirthdayCardActivity extends AppCompatActivity {
-
+    public TextView inp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_birthday_card);
+        String name = getIntent().getStringExtra("name");
+        inp = findViewById(R.id.Birthdaytext);
+        inp.setText("Happy Birthday " + name);
     }
 }
--- Birthday App/app/src/main/java/com/example/birthdayapp/MainActivity.java
+++ Birthday App/app/src/main/java/com/example/birthdayapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.birthdayapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText inp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void newActivity(View view) {
+        inp = findViewById(R.id.nameInput);
+        String name = inp.getText().toString();
+
+        Intent send = new Intent(MainActivity.this, BirthdayCardActivity.class);
+        send.putExtra("name",name);
+        startActivity(send);
+    }
+
+
 }
