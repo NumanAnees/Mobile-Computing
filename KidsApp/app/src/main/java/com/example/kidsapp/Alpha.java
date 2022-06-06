cb5aa1e867e06fe02bebaf6685edbedb085d2620
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-06-06 15:51:15 +0500

    First activity completed

---
+++ KidsApp/app/src/main/java/com/example/kidsapp/Alpha.java
@@ -0,0 +1,22 @@
+package com.example.kidsapp;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class Alpha extends AppCompatActivity {
+
+    String List[]={"Ant","Bat","Cat","Dog","Elephant",
+            "Fish","Giraffe","Horse","Ibex","Jaguar","Kangaroo","Lion","Monkey","Nilgai","Ostrich",
+            "Penguin","Quail","Rabbit","Snake","Tiger","Uguisu","Vicuna","Wolf","Xerus","Yak","Zebra"};
+
+    int Images[]= { R.drawable.ant,R.drawable.bat,R.drawable.cat ,R.drawable.dog,R.drawable.elephant, R.drawable.fish, R.drawable.giraffe,
+            R.drawable.horse,R.drawable.ibex,R.drawable.Jaguar,R.drawable.kangaroo,R.drawable.lion,R.drawable.monkey,R.drawable.nilgai,R.drawable.ostrich,
+            R.drawable.penguin,R.drawable.quail,R.drawable.rabbit,R.drawable.snake,
+            R.drawable.tiger,R.drawable.uguisu,R.drawable.vicuna,R.drawable.wolf,R.drawable.xerus,R.drawable.yak,R.drawable.zebra};
+
+   
+}
--- KidsApp/app/src/main/java/com/example/kidsapp/MainActivity.java
+++ KidsApp/app/src/main/java/com/example/kidsapp/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.kidsapp;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-
+    Button startBtn;
+    Button quizBtn;
+    Button repoBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startBtn=findViewById(R.id.learn);
+        startBtn.setOnClickListener(this);
+
+        quizBtn=findViewById(R.id.quiz);
+        quizBtn.setOnClickListener(this);
+
+        repoBtn=findViewById(R.id.repo);
+        repoBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+
+        Intent intent;
+        Uri webPage=Uri.parse("https://github.com/NumanAnees/Mobile-Computing");
+        switch (view.getId()){
+            case R.id.start:
+               // intent=new Intent(this,Alpha.class);
+                //startActivity(intent);
+                break;
+            case R.id.quiz:
+                //intent=new Intent(this,Quiz.class);
+                //startActivity(intent);
+                break;
+            case R.id.repo:
+                intent=new Intent(Intent.ACTION_VIEW,webPage);
+                startActivity(intent);
+                break;
+        }
     }
 }
